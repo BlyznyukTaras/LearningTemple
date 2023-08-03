bcf0e84cc0607b54de91b115c4b85e73986af1f6
BlyznyukTaras <onthetime731@gmail.com>
2023-08-03 16:26:47 +0300

    Testing desktop version

--- LearningTempleDesktop/src/jvmMain/kotlin/Main.kt
+++ LearningTempleDesktop/src/jvmMain/kotlin/Main.kt
@@ -1,6 +1,8 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -8,10 +10,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+val title = "Learning Temple"
 @Composable
 fun App() {
     val text by remember { mutableStateOf("Hello, World!") }
@@ -25,6 +31,12 @@ fun App() {
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
+        Image(
+            painter = painterResource("drawable/Learning Temple logo.webp"),
+            contentDescription = null,
+            contentScale = ContentScale.Inside,
+            modifier = Modifier.size(360.dp)
+        )
         Text(
             text = "Hello!!!!",
             color = bgColor,
@@ -40,7 +52,10 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = title
+    ) {
         App()
     }
 }
